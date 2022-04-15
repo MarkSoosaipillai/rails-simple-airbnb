fe29ca6d030ef685aa8f261e288bf22d70c32671
Mark Soosaipillai <markbrian14@hotmail.com>
2022-04-14 20:15:15 -0400

    work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'clearing database...'
+Flat.destroy_all
+puts 'adding new flats...'
+4.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.street_address,
+    description: Faker::House.room,
+    price_per_night: [199.99, 299.99, 399.99, 499.99].sample,
+    number_of_guests: rand(1..7)
+  )
+end
